7ec1c0d56bf07a9d2f9051826fd53dc1d844edea
Dilip Kumar R <dilipkumar2000.r@gmail.com>
2025-03-05 22:58:27 +0530

    included basic schema for database, this is for the login page

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, redirect , url_for , render_template , request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.secret_key = "beep"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime= timedelta(minutes=5)
+
+db= SQLAlchemy(app)
 
 @app.route("/")
 def home():
@@ -14,6 +21,7 @@ def login():
   if request.method == "POST":
     usr_nm = request.form["user_name"]
     usr_email= request.form["user_email"]
+    session.permanent=True
     session["user_name"] = usr_nm
     session["user_email"] = usr_email
     if usr_nm in session:
@@ -23,6 +31,7 @@ def login():
     return render_template("display.html", name=usr_nm , email=usr_email)
   
   else:
+    flash("user logged out or does not exist in current session")
     return render_template("form.html")
     
 @app.route("/logout")
